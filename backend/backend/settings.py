6b9efb55a5c31a20c75e4b9b6f09f838e5db28e8
Your Name <jtkimmn13@gmail.com>
2024-08-03 13:27:58 -0500

    Changing backend cors

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -126,10 +126,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    'https://movie-review-rec-app.vercel.app',
+    '*',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
